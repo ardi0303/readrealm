3c02f5721a79e8fc4b0c8e0c688a0b9378302353
Ardi Saputra <ardi210303@gmail.com>
2024-05-09 17:57:44 +0700

    update auth

--- src/pages/Auth/register.jsx
+++ src/pages/Auth/register.jsx
@@ -51,8 +51,8 @@ export default function Register({ backToLogin }) {
           <h1 className="text-5xl text-white font-poppinsBold">Realm.</h1>
         </div>
       </div>
-      <div className="bg-[#2A2A2A] min-h-screen flex items-center justify-center lg:w-1/3 w-full">
-        <div className="mx-4">
+      <div className="bg-[#2A2A2A] min-h-screen flex items-center justify-center lg:w-1/3 w-screen">
+        <div className="">
           <h2 className="text-2xl text-white font-poppinsBold">Create Account</h2>
           <div className="flex flex-col items-center mt-12 gap-12">
             <img src={imgAuth} alt="Auth" className="h-24 w-24" />
